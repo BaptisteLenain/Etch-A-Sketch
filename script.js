347d19b6b442f654f07f69602bfa9de349ce82b6
BaptisteLenain <baptiste.lenain@hotmail.fr>
2022-09-17 12:18:55 +0200

    grid working + hover change color

--- script.js
+++ script.js
@@ -4,20 +4,31 @@ Creer
 */
 
 const grid = document.getElementById('grid');
-let numDivPerRow = 10;
+let numDivPerRow = 70;
 let gridNumber = Math.pow(numDivPerRow,2);
+let newDiv = ''
 
 function createDivs() {
+/* This function creates the divs of the grid */
     for (let i = 0; i < gridNumber; i++) {
-        test = Math.sqrt(gridNumber);
-        console.log(test);    
-
-        const newDiv = document.createElement('div');
+        test = Math.sqrt(gridNumber);   
+        newDiv = document.createElement('div');
         newDiv.classList.add('gridPiece');
         grid.appendChild(newDiv);
-        gridCol = grid.style.gridTemplateColumns = `repeat(${Math.sqrt(gridNumber)},1fr)`;
-        gridRow = grid.style.gridTemplateRows = `repeat(${Math.sqrt(gridNumber)},1fr)`;
-    }
-}
+        gridCols = grid.style.gridTemplateColumns = `repeat(${Math.sqrt(gridNumber)},1fr)`;
+        gridRows = grid.style.gridTemplateRows = `repeat(${Math.sqrt(gridNumber)},1fr)`;
+    };
+};
 createDivs(gridNumber);
- 
+
+function colorDiv () {
+/* when click on one of the newDiv, transform the div in the selected color */
+    let gridPieces = document.querySelectorAll('.gridPiece');
+    gridPieces.forEach(gridPiece => { gridPiece.addEventListener('mousemove', () => {
+        gridPiece.style.background = 'white';
+        console.log(gridPiece);
+        
+        });
+    });
+};
+colorDiv();
